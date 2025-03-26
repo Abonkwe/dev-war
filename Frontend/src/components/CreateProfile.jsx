2b504231207c7e9584ef5476167c3580d1bcb5fb
Nfor Becky <maureen2becky@gmail.com>
2025-03-26 11:36:13 +0100

    message

---
+++ Frontend/src/components/CreateProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CreateProfile = () => {
+  return (
+    <div>
+      <form action="" className=" w-[500px] bg-gray-700 shadow-md">
+        <input type="text" 
+        placeholder='Username'/>
+
+      </form>
+    </div>
+  )
+}
+
+export default CreateProfile
