1af658dd17664606a2a025ea31fab98843bb4af9
tayuprosper <prospertayu2@gmail.com>
2025-03-26 11:29:45 +0100

    "added router"

---
+++ Frontend/src/pages/Landingpage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import CtaComponent from "../components/CtaComponent";
+const Landingpage = () => {
+    return (
+        <>
+            <Navbar />
+            <CtaComponent />
+        </>
+    )
+
+
+}
+
+
+export default Landingpage;
