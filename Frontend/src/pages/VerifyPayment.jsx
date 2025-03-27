02066db5d6d5c6309eec67f90b830def09989b9a
tayuprosper <prospertayu2@gmail.com>
2025-03-27 04:37:08 +0100

    "frontend fixes"

--- Frontend/src/pages/VerifyPayment.jsx
+++ Frontend/src/pages/VerifyPayment.jsx
@@ -1,6 +1,6 @@
  const VerifyPayment = async ()=>{
     return(
-        <div className="verify">
+        <div className="veri">
             <div className="spin"></div>
             <div className="text"></div>
         </div>
